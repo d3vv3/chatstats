15474313428e166f568148f576be9d4ed1821a1f
d3vv3 <devve.3@gmail.com>
2020-09-27 15:36:40 +0200

    feat: display selected file name and a Go button

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,21 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 // Style
 import "../styles/style.scss";
 
-class FileInput extends Component {
-  render() {
-    return (
-      <div>
-        <input type="file" name="file" id="file" class="inputfile" />
-        <label for="file">
-          <FontAwesomeIcon icon={faUpload} /> Import chat
+function FileInput(props) {
+  const [fileName, setFileName] = useState(null);
+
+  const updateFormState = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "file":
+        setFileName(value);
+        // console.log(name, event);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const submitForm = (event) => {
+    console.log("File submited!");
+    return null;
+  };
+
+  return (
+    <div className="input-container">
+      <form method="post" action="#" onSubmit={submitForm.bind(this)}>
+        <input
+          className="inputfile"
+          type="file"
+          onChange={updateFormState}
+          name="file"
+          id="file"
+          placeholder="Your chat file goes here"
+        />
+        <label htmlFor="file">
+          <FontAwesomeIcon icon={faUpload} /> Select chat
         </label>
-      </div>
-    );
-  }
+      </form>
+      <h3>{fileName}</h3>
+      {
+        //Conditionally render "Go!" button if chat file selected
+      }
+      {fileName != null ? (
+        <button className="go-button" onClick={submitForm.bind(this)}>
+          Go!
+        </button>
+      ) : null}
+    </div>
+  );
 }
 
 export default FileInput;
