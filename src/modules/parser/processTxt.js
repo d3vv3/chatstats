8767030319a5da23038ebd29137ce5f0e6297692
d3vv3 <jaimecondel@gmail.com>
2021-11-02 14:50:49 +0100

    fix: syntax error

--- src/modules/parser/processTxt.js
+++ src/modules/parser/processTxt.js
@@ -1,7 +1,7 @@
 export function processTxt(fileContentString) {
   const patt = /(\n\u200e?\[?([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4},? [0-9]{2}:[0-9]{2}:?[0-9]{0,2})\]? ?-? ([^:]*): (.*))/;
   const userLang = navigator.language || navigator.userLanguage;
-  var langUS = userLang === 'en-US';
+  var langUS = userLang === "en-US";
   // console.log(langUS);
 
   // Split messages on protptype match
@@ -10,7 +10,7 @@ export function processTxt(fileContentString) {
   var parsedList = getParsedMessageList(msgList, patt, langUS);
 
   if (parsedList === null) {
-      parsedList = getParsedMessageList(msgList, patt, !langUS);
+    parsedList = getParsedMessageList(msgList, patt, !langUS);
   }
 
   // Acommodate format to Telegram chats
@@ -19,31 +19,33 @@ export function processTxt(fileContentString) {
 }
 
 function getParsedMessageList(msgList, patt, langUS) {
-    var parsedList = [];
-    var aux = [];
-    var changeLang = false;
+  var parsedList = [];
+  var aux = [];
+  var changeLang = false;
 
-    [...msgList].forEach((elem) => {
-      if (patt.test(elem)) {
-        var msg = parseMessage(aux, langUS);
+  [...msgList].forEach(elem => {
+    if (patt.test(elem)) {
+      var msg = parseMessage(aux, langUS);
 
-        if (msg !== null) {
-          parsedList.push(msg);
-          if (isNaN(msg.date)) {
-              changeLang = true;
-              return;
-          }
+      if (msg !== null) {
+        parsedList.push(msg);
+        if (isNaN(msg.date)) {
+          changeLang = true;
+          return;
         }
-
-        aux = [];
-      } else {
-        aux.push(elem);
       }
-    });
 
-    if (changeLang) {return null;}
+      aux = [];
+    } else {
+      aux.push(elem);
+    }
+  });
 
-    return parsedList;
+  if (changeLang) {
+    return null;
+  }
+
+  return parsedList;
 }
 
 function parseMessage(msg, langUS) {
@@ -59,7 +61,7 @@ function parseMessage(msg, langUS) {
       photo:
         /.jpg \(file attached\)$/.test(msg[2]) || /\.jpg>$/.test(msg[2])
           ? "yes"
-          : null,
+          : null
     };
 
     return msgObject;
@@ -84,8 +86,8 @@ function formatDate(date, langUS) {
   var patt = /([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4}),? ([0-9]{2}:[0-9]{2})(:[0-9]{2})?/;
   var parseDate = patt.exec(date);
 
-  var day = langUS ? parseDate[2]: parseDate[1];
-  var month = langUS ? parseDate[1]: parseDate[2];
+  var day = langUS ? parseDate[2] : parseDate[1];
+  var month = langUS ? parseDate[1] : parseDate[2];
 
   var year = ("20" + parseDate[3]).slice(-4);
 
@@ -93,7 +95,7 @@ function formatDate(date, langUS) {
   month = ("0" + month).slice(-2);
 
   if (parseDate[5] === undefined) {
-      parseDate.[5] = ":00";
+    parseDate[5] = ":00";
   }
 
   // console.log(
@@ -108,13 +110,6 @@ function formatDate(date, langUS) {
   // );
 
   return new Date(
-      year +
-      "-" +
-      month +
-      "-" +
-      day +
-      "T" +
-      parseDate[4] +
-      parseDate[5]
+    year + "-" + month + "-" + day + "T" + parseDate[4] + parseDate[5]
   );
 }
