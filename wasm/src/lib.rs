8b3c390e3f287c3ce46ade03a01fb5b75d6f6e73
d3vv3 <jaimecondel@gmail.com>
2023-01-15 23:51:33 +0100

    fixup! feat(wasm): initial support

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,23 +1,23 @@
 use wasm_bindgen::prelude::*;
-use std::collections::HashMap;
-use chrono::DateTime;
+// use std::collections::HashMap;
+// use chrono::DateTime;
 
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
+// use serde::{Deserialize, Serialize};
+// use serde_json::Result;
 
 
-#[derive(Serialize, Deserialize)]
-struct Message {
-    date: DateTime,
-    from: String,
-    text: String,
-    _type: String,
-    media_type: String
-}
+// #[derive(Serialize, Deserialize)]
+// struct Message {
+//     date: DateTime,
+//     from: String,
+//     text: String,
+//     _type: String,
+//     media_type: String
+// }
 
-struct Chat {
-    messages: [Message],
-    name: String
+// struct Chat {
+//     messages: [Message],
+//     name: String
 }
 
 #[wasm_bindgen]
@@ -25,19 +25,19 @@ pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-#[wasm_bindgen]
-pub fn polarize_by_contacts(chat_object: HashMap) -> HashMap {
-    let mut messages_by_contacts = HashMap::new();
-    for (contact, &messages) in chat_object.get(&"messages") {
-        println!("Calling {}: {}", contact, call(number)); 
-    }
-}
+// #[wasm_bindgen]
+// pub fn polarize_by_contacts(chat_object: Chat) -> HashMap {
+//     let mut messages_by_contacts = HashMap::new();
+//     for (contact, &messages) in chat_object.get(&"messages") {
+//         println!("Calling {}: {}", contact, call(number)); 
+//     }
+// }
 
-if let messages_by_contacts::Value::Array(skins) = json["skins"] {
-    for skin in skins {
-        // ...
-    }
-}
+// if let messages_by_contacts::Value::Array(skins) = json["skins"] {
+//     for skin in skins {
+//         // ...
+//     }
+// }
 
 #[test]
 fn add_test() {
